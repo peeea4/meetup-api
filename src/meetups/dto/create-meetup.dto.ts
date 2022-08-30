74559e6eeca3722ff3ab7743ed43509371da6054
peeea4 <peeea4@gmail.com>
2022-08-30 19:09:15 +0300

    added create-meetup dto

--- src/meetups/dto/create-meetup.dto.ts
+++ src/meetups/dto/create-meetup.dto.ts
@@ -1,11 +1,14 @@
 import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateMeetupDto {
+    @ApiProperty({ example: "Meetup#2", description: "Name Or Theme Of The Meetup" })
+    readonly name: string;
+
     @ApiProperty({ example: "Meetup description...", description: "Description" })
     readonly description: string;
 
-    @ApiProperty({ example: "22.10.2023", description: "Date" })
-    readonly date: string;
+    @ApiProperty({ example: "22.10.2023 19.00 Zoom", description: "Time And Place" })
+    readonly timeAndPlace: string;
     
     @ApiProperty({ example: ["key1", "key2"], description: "Key Words" })
     readonly keyWords: string[];
