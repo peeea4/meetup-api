52597925106cf7f82ce6cadd35525639372c19e5
peeea4 <peeea4@gmail.com>
2022-08-30 22:57:29 +0300

    code style fixed

--- src/meetups/dto/create-meetup.dto.ts
+++ src/meetups/dto/create-meetup.dto.ts
@@ -1,15 +1,25 @@
 import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateMeetupDto {
-    @ApiProperty({ example: "Meetup#2", description: "Name Or Theme Of The Meetup" })
+    @ApiProperty({
+        example: "Meetup#2",
+        description: "Name Or Theme Of The Meetup",
+    })
     readonly name: string;
 
-    @ApiProperty({ example: "Meetup description...", description: "Description" })
+    @ApiProperty({
+        example: "Meetup description...",
+        description: "Description",
+    })
     readonly description: string;
 
-    @ApiProperty({ example: "22.10.2023 19.00 Zoom", description: "Time And Place" })
+    @ApiProperty({
+        example: "22.10.2023 19.00 Zoom",
+        description: "Time And Place",
+    })
     readonly timeAndPlace: string;
-    
+
     @ApiProperty({ example: ["key1", "key2"], description: "Key Words" })
     readonly keyWords: string[];
 }
+
