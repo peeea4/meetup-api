c0713678a534989cf797769833679ddc6c512ed0
peeea4 <peeea4@gmail.com>
2022-09-02 13:21:53 +0300

    eselint fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,15 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { SequelizeModule } from "@nestjs/sequelize";
 
+import { AuthModule } from "./auth/auth.module";
 import { Meetup } from "./meetups/meetups.model";
 import { MeetupsModule } from "./meetups/meetups.module";
+import { UserMeetups } from "./meetups/user-meetups.model";
+import { Role } from "./roles/roles.model";
+import { RolesModule } from "./roles/roles.module";
+import { UserRoles } from "./roles/user-roles.model";
 import { User } from "./users/users.model";
 import { UsersModule } from "./users/users.module";
-import { RolesModule } from "./roles/roles.module";
-import { Role } from "./roles/roles.model";
-import { UserRoles } from "./roles/user-roles.model";
-import { AuthModule } from "./auth/auth.module";
 
 @Module({
     controllers: [],
@@ -25,12 +26,12 @@ import { AuthModule } from "./auth/auth.module";
             username: process.env.POSTGRES_USER,
             password: process.env.POSTGRES_PASSWORD,
             database: process.env.POSTGRES_DB,
-            models: [User, Role, UserRoles, Meetup],
+            models: [User, Role, Meetup, UserRoles, UserMeetups],
             autoLoadModels: true,
         }),
         UsersModule,
-        MeetupsModule,
         RolesModule,
+        MeetupsModule,
         AuthModule,
     ],
 })
