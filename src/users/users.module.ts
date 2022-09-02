2d2d24121a2c8c92a2d7f36e174f26a0f39c8a94
peeea4 <peeea4@gmail.com>
2022-09-02 13:30:59 +0300

    eselint fix

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,20 +1,24 @@
 import { forwardRef, Module } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
-import { UserRoles } from "../roles/user-roles.model";
-import { Role } from "../roles/roles.model";
 
+import { AuthModule } from "../auth/auth.module";
+import { Meetup } from "../meetups/meetups.model";
+import { MeetupsModule } from "../meetups/meetups.module";
+import { UserMeetups } from "../meetups/user-meetups.model";
+import { Role } from "../roles/roles.model";
+import { RolesModule } from "../roles/roles.module";
+import { UserRoles } from "../roles/user-roles.model";
 import { UsersController } from "./users.controller";
 import { User } from "./users.model";
 import { UsersService } from "./users.service";
-import { RolesModule } from "../roles/roles.module";
-import { AuthModule } from "../auth/auth.module";
 
 @Module({
     controllers: [UsersController],
     providers: [UsersService],
     imports: [
-        SequelizeModule.forFeature([User, Role, UserRoles]),
+        SequelizeModule.forFeature([User, Role, UserRoles, Meetup, UserMeetups]),
         RolesModule,
+        MeetupsModule,
         forwardRef(() => AuthModule),
     ],
     exports: [UsersService],
